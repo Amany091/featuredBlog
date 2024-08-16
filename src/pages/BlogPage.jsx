75bc9940bfa4914586373c5fa66e77a512c858a9
amany091 <amanyzeyada10@gmail.com>
2024-08-16 22:21:11 +0300

    feat:add filter with search

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -7,13 +7,14 @@ const BlogPage = () => {
   const [blogPosts, setBlogPosts] = useState([])
   const [error, setError] = useState(null)
   const [searchParams, setSearchParams] = useSearchParams()
-  const [active, setActive] = useState('all')
+  const [active, setActive] = useState('')
 
   const handleBlogTitleFilter = (blogTitle) => {
     setActive(blogTitle)
     setSearchParams({ search: blogTitle === 'all' ? "" : blogTitle })
   }
 
+  console.log(active)
   // cache function request 
   const fetchPosts = useCallback(async () => {
     setIsLoading(true)
@@ -30,7 +31,7 @@ const BlogPage = () => {
     }
     setIsLoading(false)
   }, [])
-  
+
   useEffect(() => {
     fetchPosts()
   }, [])
@@ -39,6 +40,7 @@ const BlogPage = () => {
   const filterBlog = useMemo(() => {
     let search = searchParams.get('search') || ''
     if (!search) return blogPosts
+    setActive(search)
     return blogPosts.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()))
   }, [searchParams, blogPosts])
 
@@ -62,27 +64,29 @@ const BlogPage = () => {
             <button
               key={blog}
               onClick={() => handleBlogTitleFilter(blog)}
-              className={active === blog ? 'text-red-300' : ''}
+              className={active === blog || blog.includes(searchParams.get("search" || '') || blog === 'all')  ? 'text-red-300' : ''}
             >
               {blog}
             </button>
           ))}
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-1 ">
-          {isLoading && <div className='text-center  ' >Loading...</div>}
-          {error && <div className='text-center  ' >Error: {error.message}</div>}
-          {filterBlog.map((blog) => (
-            <Link to={`${blog.slug}`} key={blog.id}>
-              <div className="blog_card bg-gray-900 p-2 flex justify-center flex-col items-center relative  ">
-                <p className='bg-gray-400 text-center rounded-lg text-xs p-1 absolute top-0.5 left-0.5' >{blog.tag}</p>
-                <div className="blog_card_img w-12 my-2 ">
-                  <img src={blog.image} alt={blog.title} />
+          {isLoading && <div className='text-center w-full mx-auto' >Loading...</div>}
+          {error && <div className='text-center w-full mx-auto' >Error: {error.message}</div>}
+          {!error && (
+            filterBlog.map((blog) => (
+              <Link to={`${blog.slug}`} key={blog.id}>
+                <div className="blog_card bg-gray-900 p-2 flex justify-center flex-col items-center relative  ">
+                  <p className='bg-gray-400 text-center rounded-lg text-xs p-1 absolute top-0.5 left-0.5' >{blog.tag}</p>
+                  <div className="blog_card_img w-12 my-2 ">
+                    <img src={blog.image} alt={blog.title} />
+                  </div>
+                  <h2>{blog.title}</h2>
+  
                 </div>
-                <h2>{blog.title}</h2>
-
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))
+          )}
         </div>
       </div>
     </section>
